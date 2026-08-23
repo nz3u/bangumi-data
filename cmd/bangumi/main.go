f7ccf3bf2aa35ac42db08e2ec58d17020fc1b083
momo <22584870+nz3u@users.noreply.github.com>
2026-08-23 10:03:12 +0800

    ci: 新增 Release 流水线

--- cmd/bangumi/main.go
+++ cmd/bangumi/main.go
@@ -24,7 +24,8 @@ import (
 	"bangumi-subject-go/internal/pics"
 )
 
-const version = "0.1.0"
+// 版本号可在构建时通过 -ldflags "-X main.version=x.y.z" 注入。
+var version = "0.1.0"
 
 func main() {
 	if len(os.Args) < 2 {
